4a12752d3878d9030134e3c7605012abeb638f9e
fancyui <34978431+fancyui@users.noreply.github.com>
2023-02-12 01:03:14 +0800

    Update function.py

--- function.py
+++ function.py
@@ -8,17 +8,50 @@ Created on Sat Feb 11 17:09:47 2023
 import numpy as np
 import math as math
 
+# hex str request = SOI + VER + ADR + CID1 + CID2 + LENGTH + INFO + CHKSUM + EOI
+
+# hex str response = SOI + VER + ADR + RTN + LENGTH + INFO + CHKSUM + EOI
+
+# hex str soi = "37 45"
+
+# hex str ver = "11"
+
+# hex str adr: XX, 01 ~ FE
+
+# hex str cid1 = "46"
+
+# hex str cid2: XX, check CID table
+
+# hex str cid3: XX, check CID table
+
+# hex str rtn: XX, check RTN table
+
+# hex str length: XX XX
+
+# hex str info = CID2 + CID3 + "C5 5C" + Data + CRC32
+
+# hex str Data: NULL or various battery data
+
+# hex str CRC32: NULL or XX XX, CRC32 check of Data
+
+# hex str CHKSUM: XX XX
+
+# hex str EOI = "0D"
+
+
+
+
 # caculate hex value for field length
 
-# str info = "C2 06 C5 5C"
+# example: hex str info = "C2 06 C5 5C"
 
 def field_length(info):
     
-    hex_data_array = bytearray.fromhex(info)
+    data_array = bytearray.fromhex(info)
     
-    hex_data_list = list(hex_data_array)
+    data_list = list(data_array)
     
-    lenid = len(hex_data_list)
+    lenid = len(data_list)
     
     sum_lenid = math.floor(lenid/16/16) + math.floor(lenid/16) + np.mod(lenid,16)
     
@@ -35,7 +68,8 @@ def field_length(info):
 
 #caculate hex value for field chksum
 
-# str middle_data = "C2 06 C5 5C"
+# hex str middle_data = VER + ADR + CID1 + CID2 + LENGTH + INFO
+# example: middle_data = "11 01 46 C2 C0 04 C0 06 C5 5C"
 
 def field_chksum(middle_data):
     
